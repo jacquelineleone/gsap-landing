0c2ac15dfdfb00c5d49234b7611f5bb1c3c19141
jacquelineleone <jacqueline.leone02@gmail.com>
2024-07-08 22:02:34 -0300

    structure of html + styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <React.Fragment>
+      <div class="hero-image-start"></div>
+      <div class="preloader">
+        <p class="preloader__text">
+          <span>Say hello to N3</span>
+        </p>
+        <div class="preloader__background"></div>
+      </div>
+      <header class="header">
+        <p class="header__menu">Menu</p>
+        <p class="header__logo">N3</p>
+        <button class="button">Order</button>
+      </header>
+      <div class="hero__wrapper">
+        <section class="hero">
+          <div class="hero__image">
+            <img src="./hero.jpg" alt="N3 Bike" />
+          </div>
+          <div class="hero__content">
+            <p class="hero__caption">
+              <span>More details. More fun.</span>
+            </p>
+            <h1 class="hero__title">
+              <span>
+                <span>A new way to </span>
+              </span>
+              <span>
+                <span>ride in style.</span>
+              </span>
+            </h1>
+            <button class="hero__button button button--white">Explore</button>
+          </div>
+        </section>
+      </div>
+      <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/GSDevTools3.min.js"></script>
+      <script type="module" src="/app.js"></script>
+    </React.Fragment>
+  );
 }
 
 export default App;
